3e6c648ec93d59c169b0ec58c4a70676bc750fd9
Calin Radu <calinradualexandru@protonmail.com>
2021-04-23 11:39:29 +0300

    process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const knex = require("knex");
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
   client: "pg",
   connection: {
