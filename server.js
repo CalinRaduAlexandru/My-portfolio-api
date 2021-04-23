6a1c198fabda0dbec25983824136741d91e7f79c
Calin Radu <calinradualexandru@protonmail.com>
2021-04-23 11:30:04 +0300

    db

--- server.js
+++ server.js
@@ -32,6 +32,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  console.log();
+});
+
+app.get("/", (req, res) => {
+  db("messages")
+    .select()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "oupsy" });
+    });
+});
+
 app.post("/", (req, res) => {
   const { name, email, message } = req.body;
   db("messages")
